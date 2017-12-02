5ca01b1f66f90f155b6204eeadf230f9a628c192
Zach Chadwick <zachad@gmail.com>
2017-12-02 14:28:25 -0500

    Add solution to Part 2 of Day 2

--- day2.rb
+++ day2.rb
@@ -1,11 +1,36 @@
 cksum = 0
+input = File.read(ARGV[0]).lines
 
-File.read(ARGV[0]).lines.each do |line|
+puts "Part 1:"
+input.each do |line|
   row = line.split.map{|n| n.to_i}
   max = row.max
   min = row.min
 
   cksum += (max-min)
+end
+puts cksum
+
+puts "Part 2:"
+cksum = 0
+input.each do |line|
+  row = line.split.map{|n| n.to_i}.sort.reverse
+
+  stop = false
+  p row
+  row.each_with_index do |r, ix|
+    next if stop
+
+    puts "Checking: #{r}"
+    ((ix+1)..(row.length-1)).each do |ixx|
+      if (r % row[ixx]) == 0
+        division = r / row[ixx]
+        cksum += division
+        puts "Found: #{row[ixx]} goes in evenly to #{r} #{division} times"
+        stop = true
+      end
+    end
+  end
 end
 
 puts cksum
