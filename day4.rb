ec842ea634ad3a73a755578129fbefe4f04a3a5b
Zach Chadwick <zachad@gmail.com>
2017-12-04 23:21:17 -0500

    Solution to Day 4 in the usual messy fashion.

--- day4.rb
+++ day4.rb
@@ -23,3 +23,36 @@ end
  
 puts "Part 1:"
 puts counter
+
+puts "Part 2:"
+counter = 0 
+
+input.each do |line|
+  reject = false
+
+  words = line.split
+
+  words.each_with_index do |w, ix|
+    permutations = w.split("").permutation.to_a.map{|aa| aa.join}
+
+    range = (ix+1..-1)
+    next if words.slice(range).empty?
+
+    permutations.each do |per|
+      #puts "matching word #{ix}: #{per} against #{words.slice(range)}"
+      
+      found = words.slice(range).index(per)
+
+      if found != nil
+        #puts "Found duplicate: #{per} at #{found}: #{words[found]}"
+        reject = true
+      end
+      
+    end
+  end
+
+
+  counter += 1 unless reject
+end
+
+puts counter
